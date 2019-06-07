1f2a24a909ea198b554300feb0dd064a4a06dd7a
leejw1212 <47318954+leejw1212@users.noreply.github.com>
2019-06-07 14:43:12 +0900

    new version

--- app/src/main/java/com/kasania/theartofwar/Constants.kt
+++ app/src/main/java/com/kasania/theartofwar/Constants.kt
@@ -22,11 +22,11 @@ const val BackGroundImg = "Background"
 const val SetPitchValue = "PitchValue"
 const val SetPitchRate = "PitchRate"
 
-const val totalPhraseNum = 175
+const val totalPhraseNum = 174
 const val minChapterNum = 1
 const val maxChapterNum = 13
-val maxPhraseNum = intArrayOf(0,14,9,11,9,10,16,14,8,20,16,28,8,12)
-val sumOfPhrase =  intArrayOf(0,14,23,34,43,53,69,83,91,111,127,155,163,175)
+val maxPhraseNum = intArrayOf(0,14,9,10,9,10,16,14,8,20,16,28,8,12)
+val sumOfPhrase =  intArrayOf(0,14,23,33,42,52,68,82,90,110,126,154,162,174)
 val displayPhraseName  = arrayOf("총괄","1절","2절","3절","4절","5절","6절","7절","8절","9절","10절","11절","12절","13절","14절","15절","16절","17절","18절","19절","20절","21절","22절","23절","24절","25절","26절","27절","28절")
 val displayMonthName = arrayOf("JAN", "FEB", "MAR", "APR", "MAY", "JUN", "JUL", "AUG", "SEP", "OCT", "NOV", "DEC")
 
